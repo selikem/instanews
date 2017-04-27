1679be41f1fe5d947327b2ad2077ff756b1b1c62
selikem <selikem@gmail.com>
2017-04-27 10:58:13 -0700

    added fonts and styled story area for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ $(function () {
   $('#sections').on('change', function() {
     $('.loader-gif').show();
     $('.stories').empty();
+    var storyString = '';
     var section = this.value;
     var url = 'https://api.nytimes.com/svc/topstories/v2/'+ section + '.json';
     url += '?' + $.param({
@@ -20,11 +21,12 @@ $(function () {
       console.log(data);
       hideLoader();
       $.each(data.results, function(index, value) {
-        if (value.multimedia.length !== 0) {
-        $('.stories').append('<div class="story-cell"><img src="'+value.multimedia[3].url+'" class="story-image"><div class="abstract-container"><a href="'+ value.url +'" target="_blank"><p class="story-abstract">'+value.abstract+'</p></a></div></div>');
+        if (value.multimedia.length >= 5) {
+        storyString += '<div class="story-cell"><img src="'+value.multimedia[4].url+'" class="story-image"><div class="abstract-container"><a href="'+ value.url +'" target="_blank"><p class="story-abstract">'+value.abstract+'</p></a></div></div>';
         }
 
       })
+      $('.stories').append(storyString)
     }).fail(function() {
       $('.stories').append('<p>Sorry, something went wrong.</p>');
     });
